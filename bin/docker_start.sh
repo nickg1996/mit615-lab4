e265a1d714b4814c64a8bb7cb15c6408ed2f2193
Nick G <nicholas.gardiner1@outlook.com>
2020-02-25 17:19:25 -0500

    updated

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# Author: Nicholas Gardiner
 set +e
-echo “=~=~=~= Attempting to Migrate the DB =~=~=~=”
+
+echo "Attempting to migrate"
 bin/rails db:migrate 2>/dev/null
 RET=$?
 set -e
-if [ $RET -gt 0]; then
-	echo "=~=~= Migration failed; creating the database =~=~=”
-	bin/rails db:create
-	echo "=~=~=~= Migrating the Database =~=~=~=”
-	bin/rails db:migrate
-	bin/rails db:test:prepare
-	echo "=~=~=~= Seeding the database =~=~=~=”
-	bin/rails db:seed
+if [ $RET -GT 0 ]; then
+  echo "Migration Failed"
+  bin/rails db:create
+  echo "Migrating the Database"
+  bin/rails db:migrate
+  bin/rails db:test:prepare
+  echo "Seeding the database"
+  bin/rails db:seed
 fi
-echo "=~=~=~= Removing the old server PID =~=~=~=”
+echo "removing the old server"
 rm -f tmp/pids/server.pid
-echo "=~=~=~= Starting the webserver =~=~=~=”
-bin/rails server -p 3000 -b ‘0.0.0.0'
-
+echo "starting the web server"
+bin/rails server -p 3000 -b '0.0.0.0'
